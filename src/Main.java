13531ab0241c590a868041bf40564d42cf5489d7
Nicherke <nicksoncheruiyot001@gmail.com>
2024-06-29 16:57:25 +0300

    java comparison

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String a = "Hello";
+        double b = 2.3;
+        double c = 3;
+        double d = 3;
+
+        boolean try1 = a!=b;
+
+        System.out.println(try1);
+    }
+}
